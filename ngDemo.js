7658c03a9ac73cc740f57dd5b4d6073773d93fc0
cooperdow <cooperdow@users.noreply.github.com>
2016-12-04 09:50:38 -0700

    Update ngDemo.js

--- ngDemo.js
+++ ngDemo.js
@@ -24,10 +24,10 @@ ngDemo.config(function($routeProvider)
               templateUrl : 'pages/winter.html',
               controller  : 'winterController'
               })
-        .when('/onsale', 
+        .when('/products', 
               {
-              templateUrl : 'pages/onsale.html',
-              controller  : 'onsaleController'
+              templateUrl : 'pages/products.html',
+              controller  : 'productsController'
 			  });
 	});
 
@@ -36,22 +36,42 @@ ngDemo.controller('homeController', function($scope)
     $scope.images=
         [
         {
-          expedition: "Aconcagua",
-          days: "21 day expedition",
-          image: "images/aconcaguaexp.jpg"
+          name: "Cooper",
+          bio: "Glove Enthusiast",
+          image: "images/Cooper.jpg"
         },
         {
-          expedition: "Denali",
-          days: "14 day expedition",
-          image: "images/denali.jpg"
+          name: "Harrison",
+          bio: "Cross Dresser",
+          image: "images/Harrison.jpg"
+        },
+        {
+          name: "Paul",
+          bio: "Hi I'm Paul",
+          image: "images/paul.jpg"
+        },
+        {
+          name: "Robbie",
+          bio: "Duck Rider",
+          image: "images/Robbie.jpg"
+        },
+        {
+          name: "Ryan",
+          bio: "I like to watch dirt bike",
+          image: "images/Ryan.jpg"
+        },
+        {
+          name: "Sarah",
+          bio: "From South Africa",
+          image: "images/sarah.jpg"
+        },
+        {
+          name: "Sawyer",
+          bio: "Snake Charmer",
+          image: "images/Sawyer.jpg"
         }
         ];
 	});
-
-function modal()
-{
-    
-}
 
 ngDemo.controller('summerController', function($scope) 
     {
@@ -64,18 +84,18 @@ ngDemo.controller('summerController', function($scope)
         description: "An ultralight, 3-season tent for 2 that weighs in at 1 lb. 15 oz., this tent grants protection from bugs and rain at a weight one person can easily carry alone."
         },*/
         {
-        image:"images/bike1.jpg",
-        cost:"$3500",
-        name:"Cannondale Fat CAAD 1 Bike - 2016",
-        description:"With a premium lefty fork, 1x drivetrain and system integrated technology, the Fat CAAD 1 has all the fun, floaty traction of a fat bike plus the heart and soul of a racer."
+        image:"images/greysandtorreys.jpg",
+        cost:"$100",
+        name:"Greys and Torreys overnight camping trip",
+        description:"Hike the approach in the late afternoon, camping at the base of Greys peak before pushing to the summit of Greys and Torreys in the early morning."
         },
         {
-        image:"images/kayak1.jpg",
-        cost:"$850",
-        name:"Riot Magnum 80 Whitewater Kayak",
-        description:"The Magnum 80 has the right length and shape to easily punch through features without losing too much momentum and ending up going vertical. It's buoyant yet maneuverable."
+        image:"images/coloradotrail.jpg",
+        cost:"$130",
+        name:"Section(s) Hike Colorado Trail",
+        description:"Hike the Colorado Trail in sections, with price varying based on number of sections and which sections are chosen."
         },
-        {
+        /*{
         image:"images/rope1.jpg",
         cost:"$260",
         name:"Mammut Infinity 9.5mm x 70m Dry Rope",
@@ -92,7 +112,7 @@ ngDemo.controller('summerController', function($scope)
         cost:"$55",
         name:"Black Diamond Momentum Climbing Harness",
         description:"This all-purpose men's harness achieves surprising breathability and low weight while preserving the uncompromising security, comfort and durability for which Black Diamond is known."
-        }
+        }*/
         ];         
 	});
 
@@ -100,55 +120,61 @@ ngDemo.controller('winterController', function($scope)
     {
     $scope.winter =
         [
-            {
-            name: "Black Diamond Raven Ice Axe",
-            cost: "$80",
+           /* {
+            tripname: "Black Diamond Raven Ice Axe",
+            price: "$80",
             image: "images/iceaxe2.jpg",
             description: "A traditional axe for legendary ascents, the trusty Black Diamond Raven delivers quality you can feel."
             },
             {
-            name: "Black Diamond Contact Strap Crampons with ABS Plates",
-            cost: "$130",
+            tripname: "Black Diamond Contact Strap Crampons with ABS Plates",
+            price: "$130",
             image: "images/crampons2.jpg",
             description: "Compact and easy to use, the 10-point Black Diamond Contact strap-on crampons are optimized for general mountaineering, backcountry skiing and glacier walking."
             },
             {
-            name: "Rossignol Soul 7 Skis",
-            cost: "$750",
+            tripname: "Rossignol Soul 7 Skis",
+            price: "$750",
             image: "images/skis2.jpg",
             description: "Soul 7 skis deliver award-winning, freeride versatility, whether lapping freshly fallen powder or blasting through crud, and they carve responsively on hardpack snow and groomed runs."
             },
             {
-            name: "Mountain Hardwear Lamina -30 Sleeping Bag",
-            cost: "$320",
+            tripname: "Mountain Hardwear Lamina -30 Sleeping Bag",
+            price: "$320",
             image: "images/sleepingbag2.jpg",
             description: "The Mountain Hardwear Lamina -30 sleeping bag uses insulation designed to provide high loft, durability, softness and compressibility to keep you warm in frigid winter weather."
             },
             {
-            name: "Mountain Hardwear EV 3 Tent",
-            cost: "$850",
+            tripname: "Mountain Hardwear EV 3 Tent",
+            price: "$850",
             image: "images/tent2.jpg",
             description: "Spacious, light and strong, the single-wall Mountain Hardwear EV 3 Tent shelters 3 climbers throughout 4 seasons of high-altitude expeditions."
-            },
+            },*/
             {
-            name: "Backcountry Access DTS Rescue Package",
-            cost: "$320",
-            image: "images/avalanchesafety2.jpg",
-            description: "Whether you're just getting into backcountry skiing or replacing old equipment, the Backcountry Access DTS Rescue Package supplies you with 3 pieces of required gear: a transceiver, shovel and probe."
+            tripname: "Cactus Mountain Overnight",
+            price: "$45",
+            image: "images/cactusmountain.jpg",
+            description: "Overnight camping trip in Poudre Canyon on top of 'Cactus Mountain'."
             },
         ];
    });
 
-ngDemo.controller('onsaleController', function($scope) 
+ngDemo.controller('productsController', function($scope) 
     {
-    $scope.onsale=
+    $scope.products=
         [
             {
-            name: "Werner Skagit Paddle",
-            image: "images/paddle3.jpg",
-            cost: "$130 (now $80)",
-            description: "The Skagit paddle from Werner offers beginning and recreational paddlers excellent value and performance."
+            name: "Flannel Fleece Jackets",
+            image: "images/Shirt.jpg",
+            cost: "$50",
+            description: "Hand made from scratch using materials of your choice, ethically sourced and no sweatshop workers...for now."
             },
+            {
+            name: "Tent (coming soon)",
+            image: "images/tent.jpg",
+            cost: "$400",
+            description: "This is not a real tent, just a drawing."
+            },/*
             {
             name: "Teva Cedar Canyon Waxed Leather Shoes",
             image: "images/shoes3.jpg",
@@ -160,7 +186,7 @@ ngDemo.controller('onsaleController', function($scope)
             image: "images/sleepingbag3.jpg",
             cost: "$80 (now $60)",
             description: "Stuff this super-packable sleeping bag into your kayak hatch or bike pannier. It’s a master space-saver and has synthetic SpiraFil high-loft insulation that stays warm even when damp."
-            },
+            },*/
         ];
    });
 
